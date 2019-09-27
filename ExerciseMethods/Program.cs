66028bce176e8e0395a4b9d1b2037fef2f67e7b5
Gabriella <g.westberg@hotmail.se>
2019-09-27 20:40:15 +0200

    Added solutions for some of the exercises

---
+++ ExerciseMethods/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace ExerciseMethods
+{
+    class Program
+    {
+ //--------------------------------------------------------------------------------
+
+        /*static void Main(string[] args)
+        {
+            //---------------------------------------------
+            //Exercise 1, input
+            //----------------------------------------------
+            System.Console.WriteLine("Hello, what is your name?");
+            string nameinput = Console.ReadLine();
+            Hello(nameinput);
+        }
+        static void Hello(string name)
+        {
+            System.Console.WriteLine("Hello {0}, nice to see you!",name);
+        }*/
+
+ //-----------------------------------------------------------------------
+
+        /*  static void Main(string[] args)
+        {
+            //-------------------------------------------------
+            // Exercise 2, input and output. 
+            //------------------------------------------------------
+             System.Console.Write("Hello, what is you name? : "); 
+            string nameInput =Console.ReadLine();
+            System.Console.Write("How is your mood today? : "); 
+            string wellbeingInput = Console.ReadLine();
+            HelloHowAreYou(nameInput,wellbeingInput);
+            //-------------------------------------------------------
+        }
+        static void HelloHowAreYou(string name , string wellBeing)
+        {
+            System.Console.WriteLine("Hello {0}, so you are in a {1} mood today.",name,wellBeing);
+        }*/
+
+
+//-------------------------------------------------------------------------------------------------
+
+
+       /* static void Main(string[] args)
+        {
+            //----------------------------------------------------------
+            //Exercise 3; Returning the sum of two integers.
+            // (It does what it's supposed to do, though there should be a try/catch on each of the convert.int32 places.)
+            //--------------------------------------------------------
+            
+            bool onlyNumbers = false;
+            while (onlyNumbers == false)
+            {
+            try
+            {
+            System.Console.Write("The first number: "); 
+            int inputNrOne = Convert.ToInt32(Console.ReadLine());
+            System.Console.Write("The second number: "); 
+            int inputNrTwo = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("the sum of {0} and {1} is {2}",inputNrOne , inputNrTwo,Returningsum(inputNrOne,inputNrTwo));
+            onlyNumbers = true;
+            }
+            catch
+            {
+                System.Console.WriteLine("only integers are allowed");
+                onlyNumbers = false;
+            }
+            }
+
+            //-------------------------------------------------------
+        }
+        static int Returningsum(int nrOne, int nrTwo)
+        {
+            int sum = nrOne + nrTwo;
+            return sum;
+        } *7
+
+
+//---------------------------------------------------------------------------------------------------
+
+        /*
+        static void Main(string[] args)
+        {
+            //--------------------------------------------
+            // exercise 4 , add taxes (25%)
+            //--------------------------------------------
+            bool convertDouble = false;
+            while( convertDouble == false)
+            {
+            try{
+            Console.Write("Enter the price to calculate price incl. taxes: ");
+            double taxes= Convert.ToDouble (Console.ReadLine());
+            System.Console.WriteLine( "The price, including taxes will be: {0}",ReturnAddedTaxes(taxes));
+            convertDouble = true;
+            }
+            catch
+            {
+                System.Console.WriteLine("You can onnly use numbers and the comma-sign ( , )");
+                convertDouble = false;
+            }
+            }
+        }
+
+        static double ReturnAddedTaxes(double addTaxes)
+        {
+            double taxesAdded = addTaxes * 1.25;
+            taxesAdded = Math.Round(taxesAdded, 2);
+            return taxesAdded;
+        }
+         */
+
+         
+//-----------------------------------------------------------------------------------
+    
+     /*static void Main(string[] args)
+
+    //---------------------------------------------------
+    //Exercise 5 - calculate price with custom taxes.
+    //---------------------------------------------------
+
+        {
+            System.Console.Write("price: ");
+            double userPrice = Convert.ToDouble(Console.ReadLine());
+            System.Console.Write("taxes: ");
+            double userTaxes = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.WriteLine("The price, inluding the taxes is: {0}",addedInputTaxes(userPrice,userTaxes));
+        }
+
+    
+    static double addedInputTaxes(double inputPrice, double inputTaxes)
+    {
+        double priceAddedtaxes;
+        double inputTaxesPercentage = (inputTaxes * 0.01)+1;
+        priceAddedtaxes = inputPrice * inputTaxesPercentage;
+        priceAddedtaxes = Math.Round(priceAddedtaxes,2);
+        return priceAddedtaxes;
+    }*/
+        
+//------------------------------------------------------------------------------------------------------------
+
+    /*static void Main(string[] args)
+
+    //-----------------------------------------
+    // Exercise 6 - convert decimal to integer
+    //------------------------------------------
+
+    {
+        System.Console.Write("Write a decimal number: ");
+        double decimalNr = Convert.ToDouble(Console.ReadLine());
+        System.Console.WriteLine( "The number rounded at the middle will be: {0}" ,returnInt(decimalNr));
+    }
+
+    static double returnInt(double decimalNrInt)
+    {
+        double roundedDouble= Math.Round(decimalNrInt, MidpointRounding.ToEven);
+        int convertedInt = Convert.ToInt32(roundedDouble);
+
+        return convertedInt;
+    }*/
+
+//---------------------------------------------------------------------------------------------------------------
+
+
+    /*static void Main(string[] args)
+    {
+        //--------------------------------------
+        //Exercise 7 - Check age if legal or not
+        //--------------------------------------
+        System.Console.Write("Welcome to your local pub! \nHow old are you? ");
+        int LegalorNot = Convert.ToInt32(Console.ReadLine());
+        isLegalorNot(LegalorNot);
+        
+    }
+    static int isLegalorNot(int legalint)
+    {
+            if (legalint >= 18)
+            {
+                System.Console.WriteLine("You are of drinking age, welcome in!");
+            }
+            else
+            {
+                System.Console.WriteLine("You are not old enough to drink at this pub, Welcome back when you turn 18.");
+            }
+            return legalint;
+    }*/
+        
+    }
+}
--- exercise1/Program.cs
+++ exercise1/Program.cs
@@ -6,7 +6,25 @@ namespace exercise1
     {
         static void Main(string[] args)
         {
-			Console.Write("Tal 1: ");
+			//exercise();
+            //exercise2();
+            //exercise3();
+            //exercise4();
+            //exercise5();
+            //exercise6();
+            //exercise7();
+            //exercise8();
+            //exercise9();
+            //exercise10();
+            //exercise11();
+
+            
+        }
+
+        static void exercise()
+        {
+            // Din egen lösning på övning 1
+            Console.Write("Tal 1: ");
 			string inputOne = Console.ReadLine();
 			Console.Write("Tal 2: ");
 			string inputTwo = Console.ReadLine();
@@ -17,5 +35,115 @@ namespace exercise1
 
             Console.WriteLine("Summan är: " + sum);
         }
-    }
+
+        static void exercise2()
+        {
+            //konvertera decimaltal till heltal
+            System.Console.Write("Please enter a decimal number: ");
+            double deci= Convert.ToDouble(Console.ReadLine());
+            double convertedDeci= Math.Round(deci);
+            System.Console.WriteLine("{0} = {1}", deci,convertedDeci);
+            Console.ReadLine();
+        }
+
+        static void exercise3()
+        {
+            // tar in två textsträngar och skriver ut dessa i en mening.
+            System.Console.WriteLine("Welcome, to the interactive story\n");
+            System.Console.Write("Please tell us the name of your main character: ");
+            string name1= Console.ReadLine();
+
+            System.Console.Write("So this is a atory about {0}, what is the name of {0}'s best friend? : \n",name1);
+            string name2=Console.ReadLine();
+
+            System.Console.WriteLine("So this is the story about when {0} and {1} got into alot of trouble...",name1,name2);
+            Console.ReadLine();
+        }
+
+        static void exercise4()
+        {
+            //Chars
+
+        }
+
+        static void exercise5()
+        {
+            //Inmatning - utmatning, 
+            System.Console.Write("Hello!\nWhat is your name?:");
+            string name1=Console.ReadLine();
+
+            System.Console.Write("Welcome {0}! How old might you be? (answer in years): ",name1);
+            int age= Convert.ToInt32(Console.ReadLine());
+
+            //halvhyfsad uträkning för att räkna ut dagar inklusive skottår.
+            int dayslived= age*365+(age/4);
+            System.Console.WriteLine("{0}, you have been alive for {1} days",name1,dayslived); 
+        }
+
+        static void exercise6()
+        {
+            // del 1, ställ frågan om vädret.
+            //del 2 skapa alternativ för dåligt väder samt hantera stor/liten bokstav.
+            //del 3, hantera felaktig inmatning(ingen loop)
+            System.Console.Write("Is the weather nice today? Y/N answer: ");
+            string answer = Console.ReadLine();
+            answer = answer.ToUpper();
+
+            if(answer == "Y")
+            {
+                System.Console.WriteLine("\nLet's go for a picnic!");
+            }
+            else if(answer =="N")
+            {
+                System.Console.WriteLine("Let's stay inside and read a book instead.");
+            }
+            else
+            {
+                System.Console.WriteLine("Wrong input");
+            }
+        }
+
+        static void exercise7()
+        {
+            // Ingen if-sats bifogad. ;)
+        }
+
+        static void exercise8()
+        {
+            //Banksimulatorn
+        }
+
+        static void exercise9()
+        {
+            int i =0;
+            for (i = 0; i < 11; i++)
+            System.Console.WriteLine(i);
+        }
+        static void exercise10()
+        {
+            int i =0;
+            for (i = 40; i > 29; i--)
+            System.Console.WriteLine(i);
+        }
+        static void exercise11()
+        {
+            int sum=0;
+            int i=0;
+            int x = 0;
+            int y = 0;
+            System.Console.Write("skriv in tal 1:");
+            x = int.Parse(Console.ReadLine());
+            System.Console.Write("skriv in tal 2:");
+            y = int.Parse(Console.ReadLine());
+
+            for(i =x; i <= y ;i++)
+            {
+                System.Console.WriteLine("{0}\n",i);
+                sum+=i;
+            }
+            System.Console.WriteLine("{0}",sum);
+
+        }
+
+    }  
 }
